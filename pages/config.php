7168d641f96fd9e11eb42190ad3dcd234616a6bd
UroobaShameem <uroobashameem12@gmail.com>
2023-06-27 00:42:44 +0500

    commit

--- pages/config.php
+++ pages/config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('localhost','root','','book_quest') or die('connection failed');
 
 ?>
