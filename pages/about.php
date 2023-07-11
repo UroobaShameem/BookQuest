930a48d0dcc5b8afd8a018dddd6f9aa3e1b13b0d
UroobaShameem <uroobashameem12@gmail.com>
2023-07-11 23:54:03 +0500

    commit

--- pages/about.php
+++ pages/about.php
@@ -4,20 +4,42 @@ $pageTitle = "About Us";
 include 'header.php';
 ?>
 
+<style>
+body {
+    font-family: Georgia, 'Times New Roman', Times, serif ; }
+    p {
+    font-size: 1.2rem; }
+    h1 {
+    color: #15507a;
+    text-decoration: underline; }
+    h2 {
+    color: #15507a; }
+    .card {
+    
+  background-color: #ffffff;
+  transition: background-color 0.3s ease;
+}
+
+.card:hover {
+  background-color: #2a90b6; /* Change this to the desired hover color */
+}
+
+
+</style>
 
 <div class="container mt-4">
-        <h1 class="text-center">About Us</h1>
+        <h1 class="text-center">ABOUT US</h1>
         <p class="text-center mt-3">Book Quest is an exceptional online bookstore that caters to book lovers of all genres and interests. 
             With a vast collection of books from renowned authors and publishers, Book Quest offers a seamless and immersive reading 
             experience to its customers. Our platform provides a user-friendly interface that allows users to effortlessly browse 
             through various categories, discover new releases, and explore timeless classics.
         <br>At Book Quest, we believe in the power of books to inspire, educate, and entertain.</p>
         
-        <h2 class="text-center">Features</h2>
+        <h2 class="text-center">FEATURES</h2>
         <div class="row">
-            <div class="col-md-6 col-lg-4 g-5">
+            <div class="col-md-6 col-lg-4 g-5 mt-0">
                 <div class="card mb-3 border-0">
-                    <img src="asset/images/pictures/about1.png" class="card-img-top" alt="Feature 1">
+                    <img src="asset/images/pictures/about1.jpg" class="card-img-top" alt="Feature 1">
                     <div class="card-body">
                         <h5 class="card-title">Fast Delivery</h5>
                         <p class="card-text">We know how eager you are to start reading your newly 
@@ -26,7 +48,7 @@ include 'header.php';
                 </div>
             </div>
 
-            <div class="col-md-6 col-lg-4 g-5">
+            <div class="col-md-6 col-lg-4 g-5 mt-0">
                 <div class="card mb-3 border-0">
                     <img src="asset/images/pictures/about2.jpg" class="card-img-top" alt="Feature 2">
                     <div class="card-body">
@@ -36,12 +58,12 @@ include 'header.php';
                 </div>
             </div>
 
-            <div class="col-md-6 col-lg-4 g-5">
+            <div class="col-md-6 col-lg-4 g-5 mt-0">
                 <div class="card mb-3 border-0">
-                    <img src="path/to/feature3.jpg" class="card-img-top" alt="Feature 3">
+                    <img src="asset/images/pictures/about3.jpg" class="card-img-top" alt="Feature 3">
                     <div class="card-body">
-                        <h5 class="card-title">Feature 3</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h5 class="card-title">Customer Service</h5>
+                        <p class="card-text">At BookQuest, we prioritize your satisfaction. Our dedicated customer service team is always ready to assist you with any inquiries or recommendations.</p>
                     </div>
                 </div>
             </div>
